e086ee4f3c5b79ba64aa97257e6fbaf0b06455bc
Goran SD <g.cvetic23@gamail.com>
2022-03-09 15:51:35 -0800

    script.js file cleaned

--- script.js
+++ script.js
@@ -1,58 +1,47 @@
-const form     = document.querySelector('#addForm');
-const input    = document.querySelector('input');
-const ul       = document.querySelector('#toDoList');
-const clearBtn = document.querySelector('#clearList');
+const input = document.querySelector("input");
+const ul = document.querySelector("#toDoList");
+const clearButton = document.querySelector("#clearList");
 
-// Create -> li
-function createLi() {
-    //declare variable and create elements for list, inpput value, and remove button
-    const li   = document.createElement('li');
-    const span = document.createElement('span');
-    const removeBtn = document.createElement('button');
-    //Get the input value to li item
-    span.textContent = input.value;
-    //set remove button text and ID value
-    removeBtn.textContent = 'X';
-    //append the two elements to the list
-    li.appendChild(span);
-    li.appendChild(removeBtn);
-    // Show CLEAR ALL button on main page - on initial load display is set to NONE
-    clearBtn.setAttribute('style', 'display: show');
-   return li;
+function createTodoListItem() {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  const removeButton = document.createElement("button");
+  clearButton.setAttribute("style", "display: show");
+  span.textContent = input.value;
+  removeButton.textContent = "X";
+  li.appendChild(span);
+  li.appendChild(removeButton);
+  return li;
 }
-// on SUBMIT/ADD action call the function 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
 
-    const li = createLi();
-    //if the input is NOT empty execute
-    if (input.value !== ''){
-            //add li with all of its values and items to ul
-            ul.appendChild(li);
-                //Clear the input field
-                document.getElementById("addItem").value=""
-    }
+document.querySelector("#addForm").addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (input.value === "") {
+    return;
+  }
+  const li = createTodoListItem();
+  ul.appendChild(li);
+  input.value = "";
 });
-//Delete individual items from UL - to do list
-ul.addEventListener('click', (e) => {
-    if(e.target.textContent === 'X' ){
-        const button = e.target;
-        const li = button.parentNode;
-        const ul = li.parentNode;
-        ul.removeChild(li);
-        } 
-    });
-//Line through toggle on selected item    
-ul.addEventListener('mousedown', (e) => {
-     if(e.target.textContent !== '' ){
-        const line = e.target; 
-        line.onclick = () => {
-            line.classList.toggle('line')
-        }
-    }
+
+ul.addEventListener("click", (e) => {
+  if (e.target.textContent === "X") {
+    const button = e.target;
+    const li = button.parentNode;
+    li.remove();
+  }
+  if (ul.children.length === 0) {
+    clearButton.setAttribute("style", "display: none")
+  }
 });
- //Clear ALL button - clear ul
-function clearList(){
-    ul.innerHTML ="";
-    clearBtn.setAttribute('style', 'display: none');
+
+ul.addEventListener("mousedown", (e) => {
+    const todoText = e.target;
+    todoText.classList.toggle('line')
+    line.classList.toggle("line");
+});
+
+function clearList() {
+  ul.innerHTML = "";
+  clearButton.setAttribute("style", "display: none");
 }
